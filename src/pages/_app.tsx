96675b006343822222af741a4f262ec6ea2be1dd
Ali <ali.hussein.pre@gmail.com>
2022-09-07 15:09:37 +0200

    chore: install chakra & react-query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,27 @@
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import React from "react";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = React.useState(() => new QueryClient());
+
+  return (
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
